847fd298a1d7386bb187f93447fa580aade391aa
chickychips <permana.ari1@gmail.com>
2020-06-23 07:30:53 +0700

    change database connection to production

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,4 @@
-const config = require("../config/db.config.js");
+// const config = require("../config/db.config.js");
 const knex = require('knex');
 
 var types = require('pg').types;
@@ -7,15 +7,22 @@ var types = require('pg').types;
 types.setTypeParser(1114, val => val); 
 // var moment = require('moment'); // require
 // types.setTypeParser(1114, str => moment.utc(str).format());
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
-
+// const db = knex({
+//   client: config.client,
+//   connection: {
+//     host: config.host,
+//     user: config.user,
+//     password: config.password,
+//     database: config.database
+//   }
+// });
 const db = knex({
   client: config.client,
   connection: {
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    database: config.database
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
