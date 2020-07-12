015985d37fcf097f1992748583475385c3c99b26
chickychips <permana.ari1@gmail.com>
2020-07-12 08:53:08 +0700

    fix package.json

--- src/models/index.js
+++ src/models/index.js
@@ -5,25 +5,24 @@ var types = require('pg').types;
 
 // override parsing date column to timestamp
 types.setTypeParser(1114, val => val); 
-// var moment = require('moment'); // require
-// types.setTypeParser(1114, str => moment.utc(str).format());
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
-// const db = knex({
-//   client: config.client,
-//   connection: {
-//     host: config.host,
-//     user: config.user,
-//     password: config.password,
-//     database: config.database
-//   }
-// });
 const db = knex({
   client: config.client,
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
   }
 });
+// const db = knex({
+//   client: config.client,
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+//   }
+// });
 
 module.exports = db;
